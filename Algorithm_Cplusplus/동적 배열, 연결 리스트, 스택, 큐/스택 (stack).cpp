f034bef50343d1eeddeb07da4d6dac8e23451c11
janjan97 <eoen482@nate.com>
2022-10-22 23:49:00 +0900

    Add] 동적 배열과 연결 리스트 구현, 스택, 큐

---
+++ Algorithm_Cplusplus/동적 배열, 연결 리스트, 스택, 큐/스택 (stack).cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+using namespace std;
+
+// Stack (LIFO Last-In-First-Out 후입선출)
+
+// [1][2][3][4] << 밀어넣음
+// 되돌리기 (Ctrl+Z)
+
+template<typename T, typename Container = vector<T>>
+class Stack
+{
+public:
+	void push(const T& value)
+	{
+		_container.push_back(value);
+	}
+
+	void pop()
+	{
+		_container.pop_back();
+	}
+
+	T& top()
+	{
+		return _container.back();
+	}
+
+	bool empty() { return _container.empty(); }
+	int size() { return _container.size(); }
+
+private:
+	//vector<T> _container;
+	//list<T> _container;
+	Container _container;
+};
+
+int main()
+{
+	Stack<int, list<int>> s;
+
+	// 삽입
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	
+	// 비었나?
+	while (s.empty() == false)
+	{
+		// 최상위 원소
+		int data = s.top();
+		// 최상위 원소 삭제
+		s.pop();
+
+		cout << data << endl;
+	}
+	
+	int size = s.size();
+}

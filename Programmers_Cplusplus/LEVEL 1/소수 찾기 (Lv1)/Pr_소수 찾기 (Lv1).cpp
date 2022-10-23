9aa596098abf5477a10aadac7f2eb1194ea3e9ff
janjan97 <eoen482@nate.com>
2022-10-23 22:10:25 +0900

    Add] Programmers 12921 소수 찾기 (에라토스테네스의 체)

---
+++ Programmers_Cplusplus/LEVEL 1/소수 찾기 (Lv1)/Pr_소수 찾기 (Lv1).cpp
@@ -0,0 +1,25 @@
+﻿//Pr_소수 찾기 (Lv1).cpp : Programmers 12921 소수 찾기 (에라토스테네스의 체) 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+	int arr[1000000] = { 0 };
+
+	for (int i = 2; i <= n; i++)
+	{
+		if (arr[i] == 0)
+		{
+			answer++;
+			for (int j = 1; i * j <= n; j++)
+			{
+				arr[i * j] = 1;
+			}
+		}
+	}
+
+
+	return answer;
+}

1f9534fad8001c9ecf0e1d11777168f442ef5037
janjan97 <eoen482@nate.com>
2022-10-23 22:26:32 +0900

    Add] Programmers 42577 전화번호 목록 (해시/unordered_map)

---
+++ Programmers_Cplusplus/LEVEL 2/해시_전화번호 목록/Pr_해시_전화번호 목록.cpp
@@ -0,0 +1,24 @@
+﻿//Pr_해시_전화번호 목록.cpp : Programmers 42577 전화번호 목록 (해시/unordered_map)
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+	bool answer = true;
+	unordered_map<string, int>umap;
+	for (string phone : phone_book)
+	{
+		umap[phone] = 1;
+	}
+
+	for (string phone : phone_book)
+	{
+		for (int i = 1; i < phone.size(); i++)
+		{
+			if (umap[phone.substr(0, i)] == 1)
+				answer = false;
+		}
+	}
+	return answer;
+}

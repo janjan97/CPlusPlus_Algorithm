aa82c522ee5351e5d06dfec5fb4c8f0487ec7b45
janjan97 <eoen482@nate.com>
2022-10-23 21:46:32 +0900

    Add] Programmers 12906_같은 숫자는 싫어 (스택/stack)

---
+++ Programmers_Cplusplus/LEVEL 1/스택_같은 숫자는 싫어/Pr_스택_같은 숫자는 싫어.cpp
@@ -0,0 +1,24 @@
+﻿//Pr_스택_같은 숫자는 싫어.cpp : Programmers 12906_같은 숫자는 싫어 스택(stack)
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+	vector<int> answer;
+	int _size = arr.size();
+	for (int i = 0; i < _size - 1; i++)
+	{
+		if (i == 0)
+		{
+			answer.push_back(arr[i]);
+		}
+		if (arr[i] != arr[i + 1])
+		{
+			answer.push_back(arr[i + 1]);
+		}
+	}
+
+	return answer;
+}

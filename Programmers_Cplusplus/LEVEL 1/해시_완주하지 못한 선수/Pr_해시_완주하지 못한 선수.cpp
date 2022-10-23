7948f72eb94ffbb0232a0b50d4c4028886d2257a
janjan97 <eoen482@nate.com>
2022-10-23 22:13:53 +0900

    Add] Programmers 42576 완주하지 못한 선수 (해시/unordered_map)

---
+++ Programmers_Cplusplus/LEVEL 1/해시_완주하지 못한 선수/Pr_해시_완주하지 못한 선수.cpp
@@ -0,0 +1,21 @@
+﻿//Pr_해시_완주하지 못한 선수.cpp : Programmers 42576 완주하지 못한 선수 (해시/unordered_map)
+#include <string>
+#include <vector>
+#include <unordered_map> 
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+	string answer = "";
+	unordered_map <string, int>umap;
+
+	for (string name : participant)
+		umap[name]++;
+	for (string name : completion)
+		umap[name]--;
+	for (string name : participant)
+	{
+		if (umap[name] == 1)
+			answer = name;
+	}
+	return answer;
+}

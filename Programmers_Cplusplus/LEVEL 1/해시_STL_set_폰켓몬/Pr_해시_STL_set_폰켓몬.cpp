2a3b7a694ed2b8823da372ebb5c3e3239cf49e6a
janjan97 <eoen482@nate.com>
2022-10-23 22:22:08 +0900

    Add] Programmers 1845 폰켓몬 해시/STL(set 사용)

---
+++ Programmers_Cplusplus/LEVEL 1/해시_STL_set_폰켓몬/Pr_해시_STL_set_폰켓몬.cpp
@@ -0,0 +1,13 @@
+﻿//Pr_해시_STL_set_폰켓몬.cpp : Programmers 1845 폰켓몬 해시/STL(set 사용)
+#include <vector>
+#include <set> //
+
+using namespace std;
+
+int solution(vector<int> nums)
+{
+	set<int> st;
+	for (const auto& num : nums)
+		st.insert(num);
+	return min(nums.size() / 2, st.size());
+}

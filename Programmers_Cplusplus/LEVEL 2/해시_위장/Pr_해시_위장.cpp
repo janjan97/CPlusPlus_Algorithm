8a72527065a5a8b22f6340a012bd46a64a8e5ace
janjan97 <eoen482@nate.com>
2022-10-23 22:35:04 +0900

    Add] Programmers 42578 위장 (해시/unordered_map)

---
+++ Programmers_Cplusplus/LEVEL 2/해시_위장/Pr_해시_위장.cpp
@@ -0,0 +1,24 @@
+﻿//Pr_해시_위장.cpp : Programmers 42578 (해시/unordered_map)
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+	int answer = 0;
+	unordered_map<string, int>genre;
+
+	for (int i = 0; i < clothes.size(); i++)
+	{
+		genre[clothes[i][1]]++;
+	}
+
+	int tmp = 1;
+	for (auto i = genre.begin(); i != genre.end(); i++)
+	{
+		tmp = tmp * (i->second + 1);
+	}
+	answer = tmp - 1;
+
+	return answer;
+}

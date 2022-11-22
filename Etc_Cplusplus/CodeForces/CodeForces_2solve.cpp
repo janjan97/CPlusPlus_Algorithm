cb50846b855f76b1b774c7554b493922d68b4f6a
janjan97 <eoen482@nate.com>
2022-11-22 23:46:23 +0900

    Add] 코드포스 (중간 값, 문자열)

---
+++ Etc_Cplusplus/CodeForces/CodeForces_2solve.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <string>
+using namespace std;
+
+// 코드포스 1 - Medium Number
+//int main()
+//{
+//	int T;
+//	vector<int> answer;
+//
+//	cin >> T;
+//	for (int i = 0; i < T; i++)
+//	{
+//		vector<int> v;
+//		int a, b, c;
+//		cin >> a >> b >> c;
+//		v.push_back(a);
+//		v.push_back(b);
+//		v.push_back(c);
+//
+//		sort(v.begin(), v.end());
+//		answer.push_back(v[1]);
+//	}
+//
+//	for (int i = 0; i < T; i++)
+//	{
+//		cout << answer[i] << "\n";
+//	}
+//	return 0;
+//}
+
+//코드포스 2 - Atilla's Favorite Problem	
+//int main()
+//{
+//	int T;
+//	vector<int> answer;
+//	cin >> T;
+//	for (int i = 0; i < T; i++)
+//	{
+//		int n;
+//		cin >> n;
+//		
+//		string s;
+//		s.resize(n);
+//		cin >> s;
+//
+//		sort(s.begin(), s.end());
+//		char ch;
+//		ch = s[s.size()-1];
+//		
+//		int num;
+//		num = ch - 'a'+ 1;
+//		answer.push_back(num);
+//
+//	}
+//	for (int i = 0; i < T; i++)
+//	{
+//		cout << answer[i] << "\n";
+//	}
+//	return 0;
+//}
